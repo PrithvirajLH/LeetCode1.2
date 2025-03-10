0cc3570c91665aa5010d69a9003cdac53e6b17cb
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2025-03-10 10:09:36 -0500

    Time: 41 ms (37.90%) | Memory: 17.6 MB (73.69%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -13,7 +13,7 @@ class Solution:
             res += word1[i:] + word2[i:]
             return res
         
-        ans = merge(word1, word2, min(len(word1), len(word2)))
-        return ans
+        return merge(word1, word2, min(len(word1), len(word2)))
+
 
         
