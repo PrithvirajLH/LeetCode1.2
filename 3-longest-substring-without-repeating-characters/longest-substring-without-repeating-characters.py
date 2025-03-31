ac83bc042ad616887a4e3f8b750ea5db1285b654
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2025-03-31 18:35:13 -0500

    Time: 13 ms (87.48%) | Memory: 17.7 MB (96.89%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,13 +4,10 @@ class Solution:
         maxSize = 0
         seen = set()
         for r in range(len(s)):
-            if s[r] in seen:
-                while s[l] != s[r]:
-                    seen.remove(s[l])
-                    l += 1                
-                l += 1
-            else:
-                seen.add(s[r])
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1                
+            seen.add(s[r])
             maxSize = max(maxSize, (r-l) + 1)
 
         return maxSize
