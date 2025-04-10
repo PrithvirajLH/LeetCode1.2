5ae269d63479825c36753058f1017ac05398c073
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2025-04-10 13:13:40 -0500

    Time: 9 ms (91.00%) | Memory: 17.9 MB (82.84%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        map_ransom = Counter(ransomNote)
+        map_magazine = Counter(magazine)
+
+        for c in ransomNote:
+            if map_ransom[c] > map_magazine[c]:
+                return False
+        
+        return True
