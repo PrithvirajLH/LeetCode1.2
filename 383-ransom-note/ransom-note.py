3303a40e1adab1f10cf8650edd68cd485365fabc
Prithviraj Hulgur <96643920+PrithvirajLH@users.noreply.github.com>
2025-04-10 13:14:19 -0500

    Time: 14 ms (79.54%) | Memory: 17.8 MB (96.76%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -3,7 +3,7 @@ class Solution:
         map_ransom = Counter(ransomNote)
         map_magazine = Counter(magazine)
 
-        for c in ransomNote:
+        for c in set(ransomNote):
             if map_ransom[c] > map_magazine[c]:
                 return False
         
